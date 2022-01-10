f22647bccf54141a1994a89a0db91e2a536d19fa
unknown <dunning.austin@gmail.com>
2022-01-09 23:42:54 -0500

    Day 5

---
+++ 05/2.js
@@ -0,0 +1,21 @@
+input = require('./input').getInput();
+
+const points = {};
+
+for (const lineSegment of input) {
+  let [x1, y1, x2, y2] = lineSegment.match(/\d+/g).map(Number);
+
+  while (x1 !== x2 || y1 !== y2) {
+    points[`${x1},${y1}`] = (points[`${x1},${y1}`] || 0) + 1;
+
+    if (x1 < x2) x1++;
+    else if (x1 > x2) x1--;
+
+    if (y1 < y2) y1++;
+    else if (y1 > y2) y1--;
+  }
+
+  points[`${x2},${y2}`] = (points[`${x2},${y2}`] || 0) + 1;
+}
+
+console.log(Object.values(points).filter((x) => x >= 2).length);
